91979c93ef232aada4075d15268502549665967a
Nitesh Saxena <nitesh.saxena@zopsmart.com>
2022-07-27 17:38:16 +0530

    fix type mismatch

--- ems.go
+++ ems.go
@@ -156,7 +156,7 @@ func ReceiveMsg(msgConsumer TibEMSConsumer) (TibEMSMsg, error) {
 
 	// return special message in case nil message is received
 	if msg == nil {
-		return "", errors.New("nil msg received")
+		return msg, errors.New("nil msg received")
 	}
 
 	return TibEMSMsg(msg), nil
